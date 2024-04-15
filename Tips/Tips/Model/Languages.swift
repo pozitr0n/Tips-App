e3cb1f90acb03397fc49b03d6be4175c7f54e193
Роман Козарь <pozitr0n.kozarroman@gmail.com>
2024-04-15 04:27:45 +0200

    Changes the button of deleting default %, added Languages().getFlagByCountryCode() by country v.1.1

--- Tips/Tips/Model/Languages.swift
+++ Tips/Tips/Model/Languages.swift
@@ -153,11 +153,13 @@ final class Languages {
     
     func getFlagByCountryCode(country: String) -> String {
         
-        let base : UInt32 = 127397
+        let base: UInt32 = 127397
         var s = ""
+        
         for v in country.unicodeScalars {
             s.unicodeScalars.append(UnicodeScalar(base + v.value)!)
         }
+        
         return String(s)
         
     }
