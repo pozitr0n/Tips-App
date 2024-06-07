5ccf8c7c095035e7729c9813fba77f7916969fef
Роман Козарь <pozitr0n.kozarroman@gmail.com>
2024-06-08 01:17:09 +0200

    Started of developing methods for calculating

--- Tips/Tips/Model/TipsCalculations/TipsCalculations.swift
+++ Tips/Tips/Model/TipsCalculations/TipsCalculations.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+final class TipsCalculations {
+
+    // Summary Part
+    func calculateBillSummary() -> Double {
+        return 0.00
+    }
+    
+    func calculateTipSummary() -> Double {
+        return 0.00
+    }
+    
+    func calculateTotalSummary() -> Double {
+        return 0.00
+    }
+    
+    // Per Person Part
+    func calculateBillPerPerson() -> Double {
+        return 0.00
+    }
+    
+    func calculateTipPerPerson() -> Double {
+        return 0.00
+    }
+    
+    func calculateTotalPerPerson() -> Double {
+        return 0.00
+    }
+    
+}
