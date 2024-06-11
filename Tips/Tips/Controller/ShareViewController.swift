3f2925e17bf1a729738a824d301ef6b91d270579
Роман Козарь <pozitr0n.kozarroman@gmail.com>
2024-06-12 01:14:27 +0200

    Start of adding new elements to the new view

--- Tips/Tips/Controller/ShareViewController.swift
+++ Tips/Tips/Controller/ShareViewController.swift
@@ -2,15 +2,43 @@
 //  ShareViewController.swift
 //  Tips
 //
-//  Created by Raman Kozar on 12/03/2024.
+//  Created by Raman Kozar on 12/03/2024
+//  Updated by Raman Kozar on 11/06/204
 //
 
 import UIKit
 
-class ShareViewController: UIViewController {
+class ShareViewController: UIViewController, UISheetPresentationControllerDelegate {
 
+    override var sheetPresentationController: UISheetPresentationController {
+        presentationController as! UISheetPresentationController
+    }
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var mainImageView: UIImageView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        view.backgroundColor = .percentColorBackground
+        cancelButton.tintColor = .textButtonColorBackground
+       
+        mainImageView.layer.cornerRadius = 10.0
+        mainImageView.clipsToBounds = true
+        
+        sheetPresentationController.delegate = self
+        sheetPresentationController.selectedDetentIdentifier = .medium
+        sheetPresentationController.prefersGrabberVisible = true
+        sheetPresentationController.detents = [
+            .medium(),
+            .large()
+        ]
+        
+    }
+    
+    @IBAction func cancelButtonAction(_ sender: Any) {
+        self.dismiss(animated: true)
     }
 
 }
